bf5c32f5577b516697e6ae60b31655b8182c9fca
It-Shu <it.shustov@gmail.com>
2021-09-22 22:54:37 +0300

    work on 2.0 version /add href for footer

--- src/footer/Footer.js
+++ src/footer/Footer.js
@@ -12,10 +12,10 @@ function Footer() {
             <div className={s.footer}>
                 <h2 className={s.name}>Sergei Shustov</h2>
                 <div className={s.socialIcons}>
-                    <MyNetworks icon={<img className={s.icons} src={linkedinLogo} alt=""/>} />
-                    <MyNetworks icon={<img className={s.icons} src={twitterLogo} alt=""/>}/>
-                    <MyNetworks icon={<img className={s.icons} src={facebookLogo} alt=""/>}/>
-                    <MyNetworks icon={<img className={s.icons} src={githubLogo} alt=""/>}/>
+                    <MyNetworks icon={<a href="https://www.linkedin.com/in/sergei-shustov-86710b1ba/" target="_blank"><img className={s.icons} src={linkedinLogo} alt=""/></a>} />
+                    <MyNetworks icon={<a href="https://twitter.com/?lang=en" target="_blank"><img className={s.icons} src={twitterLogo} alt=""/></a>}/>
+                    <MyNetworks icon={<a href="https://www.facebook.com/" target="_blank"><img className={s.icons} src={facebookLogo} alt=""/></a>}/>
+                    <MyNetworks icon={<a href="https://github.com/It-Shu" target="_blank"><img className={s.icons} src={githubLogo} alt=""/></a>}/>
                 </div>
                 <div>Ⓒ 2021 все права защищены</div>
             </div>
