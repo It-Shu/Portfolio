21cac641e2e31d4151e1eb0520e2083dbdc7bdfa
It-Shu <it.shustov@gmail.com>
2021-09-28 01:31:43 +0300

    css

--- src/projects/Projects.js
+++ src/projects/Projects.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import s from './Projects.module.css';
-import Project from "../projects/project/Project";
-import Title from "../common/components/title/Title";
+import Project from '../projects/project/Project';
+import Title from '../common/components/title/Title';
+import TodoImage from '../assets/image/To-Do-List.jpg'
+import SocialImage from '../assets/image/Social-Network.png'
 
 
 function Projects() {
+
+    const social = {
+        backgroundImage: 'url{' + '../assets/image/To-Do-List.jpg' + '}'
+    }
+
     return (
         <div className={s.projectsBlock}>
             <div className={s.projectsContainer}>
